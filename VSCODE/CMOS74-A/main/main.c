29b40d7243fdf1b3dcd539d8c1732b3ef63ace43
f4deb <f4deb1@gmail.com>
2025-10-16 22:46:34 +0200

    VSCODE update

--- VSCODE/CMOS74-A/main/main.c
+++ VSCODE/CMOS74-A/main/main.c
@@ -1,82 +1,93 @@
-/* UART Echo Example
+/* Main */
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
+#include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+#include "sdkconfig.h"
+
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_dev.h"
+#include "esp_lcd_panel_ssd1306.h"
+
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_console.h"
+#include "esp_vfs_fat.h"
+
 #include "driver/uart.h"
 #include "driver/gpio.h"
-#include "sdkconfig.h"
-#include "esp_log.h"
 
-/**
- * This is an example which echos any data it receives on configured UART back to the sender,
- * with hardware flow control turned off. It does not use UART driver event queue.
- *
- * - Port: configured UART
- * - Receive (Rx) buffer: on
- * - Transmit (Tx) buffer: off
- * - Flow control: off
- * - Event queue: off
- * - Pin assignment: see defines below (See Kconfig)
- */
+#include "argtable3/argtable3.h"
+#include "driver/i2c_master.h"
 
-#define ECHO_TEST_TXD (CONFIG_EXAMPLE_UART_TXD)
-#define ECHO_TEST_RXD (CONFIG_EXAMPLE_UART_RXD)
-#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+/* #include "led_strip.h"
 
-#define ECHO_UART_PORT_NUM      (CONFIG_EXAMPLE_UART_PORT_NUM)
-#define ECHO_UART_BAUD_RATE     (CONFIG_EXAMPLE_UART_BAUD_RATE)
-#define ECHO_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TASK_STACK_SIZE)
+#include "../components/uartCommand/include/uartCommand.h"
+#include "../components/blueLed/include/blueLed.h"
+#include "../components/clock/include/clock.h"
+#include "../components/interface/include/interface.h"
+#include "../components/charUtils/include/charUtils.h"
+#include "../components/Oled/include/Oled.h"
+#include "../components/I2c/include/I2c.h"
+#include "../components/8IoButtonBoard/include/8IoButtonBoard.h"
 
-static const char *TAG = "UART TEST";
+*/
 
-#define BUF_SIZE (1024)
+static const char *TAG_MAIN = "Main : ";
 
-static void echo_task(void *arg)
-{
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
-    uart_config_t uart_config = {
-        .baud_rate = ECHO_UART_BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
-    };
-    int intr_alloc_flags = 0;
+void init(){
 
-#if CONFIG_UART_ISR_IN_IRAM
-    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
-#endif
+    /* Configure the peripheral according to the LED type */
+    //configure_led();
 
-    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
-    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+    //I2cBusInit();  
+    //i2c_dev_pcf8574_init();
 
-    // Configure a temporary buffer for the incoming data
-    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    //initClock();
 
-    while (1) {
-        // Read data from the UART
-        int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
-        // Write data back to the UART
-        uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, len);
-        if (len) {
-            data[len] = '\0';
-            ESP_LOGI(TAG, "Recv str: %s", (char *) data);
-        }
-    }
+
+    //UART COMMAND Task
+    /*xTaskCreate(command_uart_task, 
+                "uart_command_task", 
+                COMMAND_TASK_STACK_SIZE, 
+                NULL,
+                1, 
+                NULL);
+
+    // CLOCK Task
+    xTaskCreate(clock_task,
+                "clock_task",
+                CLOCK_TASK_STACK_SIZE,
+                NULL, 
+                4, 
+                NULL);
+
+    // IOBUTTONBOARD TASK
+    xTaskCreate(ioButtonBoard_task,
+                "IO BUTTON BOARD task",
+                IO_BUTTUN_BOARD_TASK_SIZE,
+                NULL, 
+                3, 
+                NULL);               
+              
+    // INTERFACE TASK
+    xTaskCreate(interface_task,
+                "interface_task",
+                INTERFACE_TASK_STACK_SIZE,
+                NULL,
+                2,
+                NULL);*/
 }
 
-void app_main(void)
-{
-    xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+void app_main(void){
+    init();
+    
+    //   Blink Task
+    while (1) {
+        //blinkBlueLed(getTimeBlink(), getRatioBlink());    
+    }
 }
